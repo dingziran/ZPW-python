e55de4e49532e71e147405873bc651c37d671f8f
dingziran <dingziranrr@163.com>
2013-05-23 19:55:48 +0800

    prototype of the game. simple method for exp gaining and leveling up. I need more time to describe my idea.

--- zpw.py
+++ zpw.py
@@ -1,5 +1,52 @@
 import pygame
 from pygame.locals import *
 
-from random import randint, choice
+from random import randint
 from gameobjects.vector2 import Vector2
+SCREEN_SIZE=(640,480)
+LOCATION=(0,0)
+TXT_HEIGHT=20
+
+class Character(object):
+    
+    def __init__(self,name):
+        self.name=name
+        self.level=0
+        self.exp=0
+        
+    def render(self,surface):
+        font=pygame.font.SysFont("courier new",16,True)
+        nameTxt=font.render(self.name,True,(0,0,0))
+        levelTxt=font.render("level is "+str(self.level),True,(0,0,0))
+        expTxt=font.render("exp is " +str(self.exp),True,(0,0,0))
+        surface.blit(nameTxt,LOCATION)
+        surface.blit(levelTxt,(LOCATION[0],LOCATION[1]+TXT_HEIGHT))
+        surface.blit(expTxt,(LOCATION[0],LOCATION[1]+TXT_HEIGHT*2))
+        
+    def addExp(self,num):
+        self.exp+=num
+        if(self.exp>100):
+            self.exp-=100
+            self.level+=1
+        
+def run():
+    pygame.init()    
+    screen = pygame.display.set_mode(SCREEN_SIZE, 0,32)
+    clock=pygame.time.Clock()
+    char=Character("Dingziran")
+    while True:
+        for event in pygame.event.get():
+            if event.type ==QUIT:
+                return
+            
+        time_passed=clock.tick(30)
+        screen.fill((255, 255, 255))
+        char.addExp(1)
+        char.render(screen)
+        pygame.display.update()
+        
+if __name__ == "__main__":
+
+    run()
+        
+            
